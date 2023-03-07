0171e26f71d5eb0efda3d890158685ac54ca33d7
Alex-Mak-MCW <77552818+Alex-Mak-MCW@users.noreply.github.com>
2023-03-06 17:50:03 -0700

    done?

--- client/admin.py
+++ client/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from rest_framework.authtoken.admin import TokenAdmin
 
 # Register your models here.
-TokenAdmin.raw_id_fields = ('user',)
+TokenAdmin.raw_id_fields = ('user',) 
